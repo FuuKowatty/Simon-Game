b0d8d95fdda1a70edbf7055ce7855154a1f63c67
FuuKowatty <miedzio.2000@gmail.com>
2022-09-15 14:59:42 +0200

    DisplaySeq func

--- app.js
+++ app.js
@@ -1,29 +1,30 @@
 const simonBtns = document.querySelectorAll('.simonBtn button');
 const startBtn = document.querySelector('.start');
 const scoreContainer = document.querySelector('.score');
+const btnContainer = document.querySelectorAll('.simonBtn');
 let score = 0;
 
 simonBtns.forEach(e => e.addEventListener('click', () => {
-    clickAnimation(e);
+    clickAnimation(e.parentNode);
+
+    return e.parentNode
 }))
 
 
 startBtn.addEventListener('click', () => {
-    score++;
-    scoreContainer.innerText = score;
+    score++; scoreContainer.innerText = score;
     const result = drawSeq();
-   
+    displaySeq(result);
+    clickPhase(result)
 })
 
-function clickAnimation(item) {
-    let parentItem = item.parentNode;
+function clickAnimation(parentItem) {
     parentItem.classList.add('clicked');
-    setTimeout( () => {parentItem.classList.remove('clicked')}, 400)
+    setTimeout( () => {parentItem.classList.remove('clicked')}, 800)
     
 }
 
 function drawSeq() {
-    let rounds = 0;
     let result = []
     const Simon = {
         1: 'yellow',
@@ -31,10 +32,29 @@ function drawSeq() {
         3: 'blue',
         4: 'green'
     }
-    while(rounds < score) {
+
+    for(let i=0; i<score;i++){
         result.push( Simon[ Math.floor(Math.random() * 4) + 1 ] );
-        rounds++
     }
 
     return result;
+}
+
+function displaySeq(result) {
+
+    let arr = result;
+    setInterval( () => {
+        if(arr.length === 0)  clearInterval();
+        let chose = arr.shift();
+        btnContainer.forEach(e => {
+            let trigger = e.dataset.color;
+            if(trigger === chose ) {
+                clickAnimation(e)
+            }
+        })
+    }, 1000)
+}
+
+function clickPhase(arr) {
+    
 }
